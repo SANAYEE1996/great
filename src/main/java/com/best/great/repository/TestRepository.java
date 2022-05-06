091c21f8363096d878091fe325f67c22fb19d9cf
SANAYEE <dudtkd0219@gmail.com>
2022-05-06 19:48:27 +0900

    jpa 연결위한 Repository

---
+++ src/main/java/com/best/great/repository/TestRepository.java
@@ -0,0 +1,12 @@
+package com.best.great.repository;
+
+import com.best.great.entity.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<Test, String> {
+
+
+
+
+
+}
