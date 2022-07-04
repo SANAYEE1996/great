db19bb3ec5953803ac2759bcbbdf0eb15a05f2c1
SANAYEE <dudtkd0219@gmail.com>
2022-07-05 00:15:50 +0900

    광고 채널 객체

---
+++ src/main/java/com/best/great/entity/Channel.java
@@ -0,0 +1,30 @@
+package com.best.great.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "channel")
+@Data
+public class Channel {
+
+    @Id
+    private String ch_url;
+
+    private int monthView;
+    private String ch_name;
+    private int sub;
+    private String regdate;
+    private Double totview;
+    private String category;
+    private String contact;
+    private String fan;
+    private int insta;
+    private int upload;
+    private String img;
+    private String tag;
+    private int clust;
+    private Double adavgview;
+    private int adcount;
+}
