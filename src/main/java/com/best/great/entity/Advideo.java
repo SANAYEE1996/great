e72f9f95fa8389a6cb8aa5e0c604ae1941d17e3b
SANAYEE <dudtkd0219@gmail.com>
2022-07-04 19:46:08 +0900

    유튜브 db 마이그레이션 시작

--- src/main/java/com/best/great/entity/Advideo.java
+++ src/main/java/com/best/great/entity/Advideo.java
@@ -8,5 +8,16 @@ import javax.persistence.Entity;
 @Data
 public class Advideo {
     private String ad_url;
-
+    private String ch_url;
+    private String name;
+    private String upday;
+    private String category;
+    private int likeCnt;
+    private int dislike;
+    private double ratio;
+    private int click;
+    private int comment;
+    private String hashtag;
+    private String descript;
+    private Double foreign;
 }
