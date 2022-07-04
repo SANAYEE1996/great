a82ca56a1b98431abcd42cfbebf184bbf6a98dc0
SANAYEE <dudtkd0219@gmail.com>
2022-07-05 00:15:31 +0900

    게시판 db 연결 수정

--- src/main/java/com/best/great/repository/BoardRepository.java
+++ src/main/java/com/best/great/repository/BoardRepository.java
@@ -1,8 +1,6 @@
 package com.best.great.repository;
 
 import com.best.great.entity.Board;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
--- src/main/java/com/best/great/service/BoardService.java
+++ src/main/java/com/best/great/service/BoardService.java
@@ -4,11 +4,9 @@ import com.best.great.entity.Board;
 import com.best.great.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class BoardService {
