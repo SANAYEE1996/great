78abdbb28f5c5de5511d2fcf04e54e8179bc0dee
SANAYEE <dudtkd0219@gmail.com>
2022-07-18 00:34:15 +0900

    초성 비교 서비스

--- src/main/java/com/best/great/service/CompareService.java
+++ src/main/java/com/best/great/service/CompareService.java
@@ -1,4 +1,25 @@
 package com.best.great.service;
 
+
+import com.best.great.entity.Channel;
+import com.best.great.repository.ChannelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CompareService {
+
+    @Autowired
+    ChannelRepository adVideoRepository;
+
+    public List<Channel> getSearchResult(String searchString){
+        List<Channel> exam = adVideoRepository.findAll();
+        return exam;
+    }
+
 }
