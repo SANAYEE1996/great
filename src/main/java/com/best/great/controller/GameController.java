582eb7fe91d04724adee6ac8a896ee299f093449
SANAYEE <dudtkd0219@gmail.com>
2022-08-05 00:53:13 +0900

    game url 넘기는 컨트롤러

---
+++ src/main/java/com/best/great/controller/GameController.java
@@ -0,0 +1,15 @@
+package com.best.great.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/game")
+public class GameController {
+
+    @GetMapping("/wordle")
+    public String wordle(){
+        return "game/wordle";
+    }
+}
