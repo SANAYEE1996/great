8fbceee4272ca68e83f566c6804e03e55701903e
SANAYEE <dudtkd0219@gmail.com>
2022-09-19 23:50:19 +0900

    광고 동영상 페이징 서비스

--- src/main/java/com/best/great/service/AdvideoService.java
+++ src/main/java/com/best/great/service/AdvideoService.java
@@ -5,6 +5,8 @@ import com.best.great.repository.AdvideoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,9 +18,13 @@ public class AdvideoService {
     @Autowired
     AdvideoRepository advideoRepository;
 
-    public List<Advideo> getAllAdVideoByChUrl(String ch_url){
+    //public List<Advideo> getAllAdVideoByChUrl(String ch_url){
+    //    return advideoRepository.findAllByChannelUrl(ch_url);
+    //}
 
-        return advideoRepository.findAllByChannelUrl(ch_url);
+    public Page<Advideo> getAdvideoList(String ch_url,Pageable pageable){
+
+        return advideoRepository.findAllByChannelUrl(ch_url, pageable);
     }
 
 }
