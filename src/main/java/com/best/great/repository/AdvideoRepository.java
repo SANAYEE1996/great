05d2798c1bb4fcc9dd478ae5a011c473efbf2c72
SANAYEE <dudtkd0219@gmail.com>
2022-09-19 23:49:52 +0900

    페이징 추가한 특정 칼럼을 찾는 메서드

--- src/main/java/com/best/great/repository/AdvideoRepository.java
+++ src/main/java/com/best/great/repository/AdvideoRepository.java
@@ -1,10 +1,12 @@
 package com.best.great.repository;
 
 import com.best.great.entity.Advideo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AdvideoRepository extends JpaRepository<Advideo, String> {
-    List<Advideo> findAllByChannelUrl(String ch_url);
+    Page<Advideo> findAllByChannelUrl(String ch_url, Pageable pageable);
 }
