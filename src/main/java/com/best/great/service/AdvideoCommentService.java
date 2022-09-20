3ab7fbdebd5cc561005750b6cd698c93b848fa0a
SANAYEE <dudtkd0219@gmail.com>
2022-09-21 00:02:17 +0900

    댓글만 가져오는 서비스에 메서드 달랑 한개

---
+++ src/main/java/com/best/great/service/AdvideoCommentService.java
@@ -0,0 +1,19 @@
+package com.best.great.service;
+
+import com.best.great.repository.AdvideoCommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdvideoCommentService {
+
+    @Autowired
+    AdvideoCommentRepository advideoCommentRepository;
+
+    public String getComment(String url){
+        if(advideoCommentRepository.findById(url).isPresent()){
+            return advideoCommentRepository.findById(url).get().getComment();
+        }
+        return "";
+    }
+}
