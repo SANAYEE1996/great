69a7c4844ad9abc36cc3a59267639051f6fa768a
SANAYEE <dudtkd0219@gmail.com>
2022-09-20 23:44:43 +0900

    주키로 검색하는 것을 메서드로 지정하는것 보다 그냥 findById 를 쓰자

--- src/main/java/com/best/great/repository/AdvideoRepository.java
+++ src/main/java/com/best/great/repository/AdvideoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdvideoRepository extends JpaRepository<Advideo, String> {
     Page<Advideo> findAllByChannelUrl(String ch_url, Pageable pageable);
 }
