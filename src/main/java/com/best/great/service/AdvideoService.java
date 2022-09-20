8284722fe576d7f1d7dcbc6f0ef66142a2e0c19c
SANAYEE <dudtkd0219@gmail.com>
2022-09-20 23:45:03 +0900

    광고영상 세부 정보

--- src/main/java/com/best/great/service/AdvideoService.java
+++ src/main/java/com/best/great/service/AdvideoService.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AdvideoService {
     private static final Logger log = LoggerFactory.getLogger(AdvideoService.class);
@@ -19,6 +21,11 @@ public class AdvideoService {
     public Page<Advideo> getAdvideoList(String ch_url,Pageable pageable){
 
         return advideoRepository.findAllByChannelUrl(ch_url, pageable);
+    }
+
+    public Optional<Advideo> getDetailAdvideo(String ad_url){
+        log.info("받아온 광고 동영상 url : {}", ad_url);
+        return advideoRepository.findById(ad_url);
     }
 
 }
