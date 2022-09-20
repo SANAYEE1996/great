fb322d067d2b929c5fbb07384c97850551f40506
SANAYEE <dudtkd0219@gmail.com>
2022-09-21 00:14:15 +0900

    댓글 컨트롤러

--- src/main/java/com/best/great/controller/AdvideoController.java
+++ src/main/java/com/best/great/controller/AdvideoController.java
@@ -2,6 +2,7 @@ package com.best.great.controller;
 
 
 import com.best.great.entity.Advideo;
+import com.best.great.service.AdvideoCommentService;
 import com.best.great.service.AdvideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/advideo")
@@ -20,10 +19,15 @@ public class AdvideoController {
     @Autowired
     AdvideoService advideoService;
 
+    @Autowired
+    AdvideoCommentService advideoCommentService;
+
     @GetMapping("/detail")
     public String getAdvideoDetail(Model model, @RequestParam("ad_url") String ad_url){
         Advideo getAdvideoDetailList = advideoService.getDetailAdvideo(ad_url).get();
+        String comment = advideoCommentService.getComment(ad_url);
         model.addAttribute("detail",getAdvideoDetailList);
+        model.addAttribute("comment",comment);
 
         return "youtube/adVideoDetailPage";
     }
