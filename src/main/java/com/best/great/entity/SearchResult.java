1ffa9a8ce54fa5e06fdf2e34407e11d50f99f991
SANAYEE <dudtkd0219@gmail.com>
2022-09-23 19:01:18 +0900

    필요없는 어노테이션 제거

--- src/main/java/com/best/great/entity/SearchResult.java
+++ src/main/java/com/best/great/entity/SearchResult.java
@@ -2,13 +2,9 @@ package com.best.great.entity;
 
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 
 @Data
-@Getter
-@Setter
 public class SearchResult {
     private String title;
     private String link;
