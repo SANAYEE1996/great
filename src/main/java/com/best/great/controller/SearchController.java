e64f3c9da6486a381f4cf560aac974b1ac3f2fb8
SANAYEE <dudtkd0219@gmail.com>
2022-09-23 18:59:30 +0900

    왜 autowired 안했나요?

--- src/main/java/com/best/great/controller/SearchController.java
+++ src/main/java/com/best/great/controller/SearchController.java
@@ -5,6 +5,7 @@ import com.best.great.entity.SearchResult;
 import com.best.great.service.Crawling;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ public class SearchController {
 
     private static final Logger log = LoggerFactory.getLogger(SearchController.class);
 
+    @Autowired
+    public Crawling craw;
+
     @GetMapping("")
     public String searchPage() {
         log.debug("기본 검색 화면");
@@ -30,7 +34,6 @@ public class SearchController {
     @RequestMapping(value = "/result", method = RequestMethod.POST)
     public String search(@RequestParam("keyword") String keyword, Model models){
 
-        Crawling craw = new Crawling();
         ArrayList<SearchResult> list = new ArrayList<>(craw.searchCrawling(keyword));
 
         keyword = "입력 받은 검색어 : " + keyword;
