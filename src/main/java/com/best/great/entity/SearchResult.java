d59e4f6c0b094095f8c4124b401a03f1e6c41e4a
SANAYEE <dudtkd0219@gmail.com>
2022-09-23 19:04:47 +0900

    데이터 전송용에 적합하지 않아 그냥 담는 객체용도로 바꿈

--- src/main/java/com/best/great/entity/SearchResult.java
+++ src/main/java/com/best/great/entity/SearchResult.java
@@ -1,13 +1,16 @@
 package com.best.great.entity;
 
 
-import lombok.Data;
-
-
-@Data
 public class SearchResult {
     private String title;
     private String link;
     private String text;
     private int freq;
+
+    public SearchResult(String title, String link, String text, int freq){
+        this.title = title;
+        this.link = link;
+        this.text = text;
+        this.freq = freq;
+    }
 }
