d9a6c06142018ac4ca70205d06811185c24381bd
SANAYEE <dudtkd0219@gmail.com>
2022-12-16 01:45:02 +0900

    Speech to Text 초기화

---
+++ src/main/java/com/best/great/util/STT.java
@@ -0,0 +1,84 @@
+package com.best.great.util;
+
+import com.google.gson.Gson;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class STT {
+    static public void main ( String[] args ) {
+        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition";
+        String accessKey = "blind";    // 발급받은 API Key
+        String languageCode = "korean";     // 언어 코드
+        String audioFilePath = "path";  // 녹음된 음성 파일 경로
+        String audioContents = null;
+
+        Gson gson = new Gson();
+
+        Map<String, Object> request = new HashMap<>();
+        Map<String, String> argument = new HashMap<>();
+
+        try {
+            Path path = Paths.get(audioFilePath);
+            byte[] audioBytes = Files.readAllBytes(path);
+            audioContents = Base64.getEncoder().encodeToString(audioBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        argument.put("language_code", languageCode);
+        argument.put("audio", audioContents);
+
+        request.put("argument", argument);
+
+        URL url;
+        Integer responseCode = null;
+        String responBody = null;
+        try {
+            url = new URL(openApiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Authorization", accessKey);
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(gson.toJson(request).getBytes("UTF-8"));
+            wr.flush();
+            wr.close();
+            System.out.println("under Code Do Not Work ..zzz");
+            responseCode = con.getResponseCode();
+            InputStream is = con.getInputStream();
+            byte[] buffer = new byte[is.available()];
+            int byteRead = is.read(buffer);
+            responBody = new String(buffer);
+
+            System.out.println("[responseCode] " + responseCode);
+            System.out.println("[responBody]");
+            System.out.println(responBody);
+            String[] bodyArray = responBody.split(",");
+            String[] bodyContentArray;
+            for(int i = 0; i < bodyArray.length; i++) {
+                bodyContentArray = bodyArray[i].split(":");
+//            	System.out.println(bodyContentArray[0] + " " +bodyContentArray[1]);
+                System.out.println(bodyArray[i]);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
