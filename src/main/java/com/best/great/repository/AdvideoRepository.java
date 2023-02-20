67d706c9a2c73860fcf38047cdf4b453deb764a6
SANAYEE <dudtkd0219@gmail.com>
2023-02-20 22:32:12 +0900

    메서드 수정

--- src/main/java/com/best/great/repository/AdvideoRepository.java
+++ src/main/java/com/best/great/repository/AdvideoRepository.java
@@ -3,16 +3,12 @@ package com.best.great.repository;
 import com.best.great.entity.Advideo;
 import com.best.great.entity.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AdvideoRepository extends JpaRepository<Advideo, Long> {
 
-    @Query(value="select x from advideo x "
-                +" where x.channel.channelUrl = :url")
-    List<Advideo> findAllAdvideosByChannel(@Param(value = "url") String channelUrl);
+    List<Advideo> getAdvideosByChannel(Channel targetChannel);
 
     Advideo findAdvideoByAdUrl(String ad_url);
 }
