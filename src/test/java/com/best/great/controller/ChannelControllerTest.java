844a2e1f3863472031bb6595c256e4294917d2c7
SANAYEE <dudtkd0219@gmail.com>
2023-03-22 00:26:51 +0900

    @WebMvcTest 활용한 테스트 코드 작성 훨씬 빠르고 가볍지만, 서비스에 많이 의존하는 컨트롤러의 경우 그 많으 서비스들을 다  mock 으로 등록해야 해서 어느정도 규모가 있으면 시간이 많이 걸릴것 같다.

---
+++ src/test/java/com/best/great/controller/ChannelControllerTest.java
@@ -0,0 +1,82 @@
+package com.best.great.controller;
+
+import com.best.great.dto.ChannelDto;
+import com.best.great.service.AdvideoService;
+import com.best.great.service.ChannelService;
+import com.best.great.service.CompareService;
+import com.best.great.util.DtoConverter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ChannelController.class)
+class ChannelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private DtoConverter dtoConverter;
+
+    @MockBean
+    private ChannelService channelService;
+
+    @MockBean
+    private CompareService compareService;
+
+    @MockBean
+    private AdvideoService advideoService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @DisplayName("채널 상세정보 잘 가져오는지 테스트")
+    @Test
+    void getChannelDetailInformationTest() throws Exception {
+        String channelUrl = "UCBkyj16n2snkRg1BAzpovXQ";
+
+        ChannelDto channelDto = new ChannelDto(channelUrl,0,"",0,"",0.0,"","","",0,0,"","",0,0.0,0);
+
+        String content = objectMapper.writeValueAsString(channelDto);
+
+        mockMvc.perform(post("/channel/detail/info")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    void getChannelAdVideoList() {
+    }
+
+    @Test
+    void filterPage() {
+    }
+}
